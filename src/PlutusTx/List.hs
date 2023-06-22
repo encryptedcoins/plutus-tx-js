0feab88fc5723c791ac517d583fdd64b50fe63a2
encoins <111982001+encryptedcoins@users.noreply.github.com>
2023-06-22 18:42:44 +0200

    Add drop function for lists

--- src/PlutusTx/List.hs
+++ src/PlutusTx/List.hs
@@ -150,6 +150,13 @@ take n _      | n <= 0 =  []
 take _ []              =  []
 take n (x:xs)          =  x : take (Builtins.subtractInteger n 1) xs
 
+{-# INLINABLE drop #-}
+-- | Plutus Tx version of 'Data.List.drop'.
+drop :: Integer -> [a] -> [a]
+drop n xs     | n <= 0 = xs
+drop _ []              = []
+drop n (_:xs)          = drop (Builtins.subtractInteger n 1) xs
+
 {-# INLINABLE nub #-}
 -- | Plutus Tx version of 'Data.List.nub'.
 nub :: (Eq a) => [a] -> [a]
